4ebdc4467f4c7a1a6fd0fabbcf6f4267af387be0
KenjiRage <68568868+KenjiRage@users.noreply.github.com>
2022-11-21 18:48:26 +0100

    Update generadorexcusas.js

--- generadorexcusas.js
+++ generadorexcusas.js
@@ -1,19 +1,18 @@
-let sujeto = ["Mi perro",  "Mi gato", "Mi hermano", "Una cabra", "Un alien"];
-let accion = ["se comió", "rompió", "borró", "robó", "atacó"]
-let cosa = ["mis deberes", "mi ordenador", "mis apuntes", "mi teléfono", "mi portatil"]
-let cuando = ["por la mañana", "durante la noche", "cuando estaba durmiendo", "cuando salí a pasear", "cuando estaba en el baño"]
+let subject = ["Mi perro",  "Mi gato", "Mi hermano", "Una cabra", "Un alien"];
+let action = ["se comió", "rompió", "borró", "robó", "atacó"]
+let thing = ["mis deberes", "mi ordenador", "mis apuntes", "mi teléfono", "mi portatil"]
+let when = ["por la mañana", "durante la noche", "cuando estaba durmiendo", "cuando salí a pasear", "cuando estaba en el baño"]
 
-function generadorExcusa(arr){
-    let nuevaExcusa = sujeto[Math.floor(Math.random() * arr.length)] + " " + 
-                    accion[Math.floor(Math.random() * arr.length)] + " " + 
-                    cosa[Math.floor(Math.random() * arr.length)] + " " + 
-                    cuando[Math.floor(Math.random() * arr.length)]
-    return nuevaExcusa
+function generatorExcuse(arr){
+    let newExcuse = subject[Math.floor(Math.random() * arr.length)] + " " + 
+                    action[Math.floor(Math.random() * arr.length)] + " " + 
+                    thing[Math.floor(Math.random() * arr.length)] + " " + 
+                    when[Math.floor(Math.random() * arr.length)]
+    return newExcuse
 }
 
-function agregarFrase(){
-    document.getElementById("excusa").innerHTML = generadorExcusa(sujeto, accion, 
-    cosa, cuando);
+function addPhrase(){
+    document.getElementById("excusa").innerHTML = generatorExcuse(subject, action, thing, when);
 }
 
-window.onload = function(){agregarFrase()};
+window.onload = function(){addPhrase()};
